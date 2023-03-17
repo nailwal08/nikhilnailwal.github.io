fdee2af1c6a0d31bd54efcd5da336f4ff5271dbb
NIKHIL NAILWAL <78683932+nailwal08@users.noreply.github.com>
2023-03-17 22:21:57 +0530

    portfolio
    hi

--- js/script.js
+++ js/script.js
@@ -29,4 +29,4 @@ ScrollReveal().reveal('.home-content, .heading', { origin:'top' });
 ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin:'bottom' });
 ScrollReveal().reveal('.home-content h1, .about-img', { origin:'left' });
 ScrollReveal().reveal('.home-content p, .about-content', { origin:'right' });
-const typed=new Typed('.multiple-text',{strings:['Web Devloper','youtuber'],typeSpeed: 100,backSpeed: 100,backDelay: 1000, loop: true});
+const typed=new Typed('.multiple-text',{strings:['Web Devlopment.','Team Management.','Sketching.','Photography'],typeSpeed: 100,backSpeed: 100,backDelay: 1000, loop: true});
